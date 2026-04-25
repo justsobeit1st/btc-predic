970344c114db98a1c005a19fe86ea92dfeb92206
justsobeit1st <justsobeit1st@gmail.com>
2026-04-25 12:32:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/btc-predic/', // 👈 REQUIRED
+  base: '/btc-predic/', // 🔥 MUST match your repo name
 })
